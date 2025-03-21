4a90a5e66c8225225f81a93e886917fa0ac8da49
MObinXIV <mobinxiv@gmail.com>
2025-03-21 08:00:35 +0200

    add some work in the entity

--- src/main/java/com/mobi/ecommerce/order/Order.java
+++ src/main/java/com/mobi/ecommerce/order/Order.java
@@ -1,30 +1,29 @@
 package com.mobi.ecommerce.order;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mobi.ecommerce.order_product.OrderProduct;
 import com.mobi.ecommerce.user.User;
 import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static jakarta.persistence.EnumType.STRING;
 
 @Entity(name = "Order")
-@Table (name = "order")
+@Table (name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", updatable = false, nullable = false)
     private  UUID id;
-    @Column(
-            name = "description",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
-    private String description;
     @Enumerated(STRING)
     @Column(name = "status", nullable = false)
     private OrderStatus orderStatus = OrderStatus.PENDING;
@@ -33,7 +32,7 @@ public class Order {
     @Column(name = "payment_method", nullable = false)
     private PaymentMethod paymentMethod;
     @Column(name = "total_price", nullable = false)
-    private BigDecimal totalPrice;
+    private BigDecimal totalPrice=BigDecimal.ZERO;
     @CreatedDate
     @Column ( name = "createdAt", nullable = false,updatable = false)
     private LocalDateTime createdAt;
@@ -48,9 +47,123 @@ public class Order {
 
     @JsonIgnore
     @ManyToOne
-    @JoinColumn(name = "user_idO", nullable = false, referencedColumnName = "id",
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id",
             foreignKey = @ForeignKey(name = "user_order_fk"))
     private User user;
 
+    @OneToMany(mappedBy = "order", orphanRemoval = true)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
 
+    public List<OrderProduct> getOrderProducts() {
+        return orderProducts;
+    }
+
+    public void setOrderProducts(List<OrderProduct> orderProducts) {
+        this.orderProducts = orderProducts;
+    }
+
+    public Order(UUID id, OrderStatus orderStatus, PaymentMethod paymentMethod, BigDecimal totalPrice, LocalDateTime createdAt, LocalDateTime lastModifiedDate, Integer shippingFee, String phoneNumber, User user) {
+        this.id = id;
+        this.orderStatus = orderStatus;
+        this.paymentMethod = paymentMethod;
+        this.totalPrice = totalPrice;
+        this.createdAt = createdAt;
+        this.lastModifiedDate = lastModifiedDate;
+        this.shippingFee = shippingFee;
+        this.phoneNumber = phoneNumber;
+        this.user = user;
+    }
+
+    public Order() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public Integer getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(Integer shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    void addOrderProduct(OrderProduct orderProduct){
+        if(orderProduct != null &&!orderProducts.contains(orderProduct)){
+            orderProducts.add(orderProduct);
+            orderProduct.setOrder(this);
+            totalPrice = totalPrice.add(orderProduct.getPrice());
+
+        }
+    }
+    public boolean isLocked() {
+        return createdAt.plusDays(2).isBefore(LocalDateTime.now());
+    }
+    void removeOrderProduct(OrderProduct orderProduct){
+        orderProducts.remove(orderProduct);
+        totalPrice = totalPrice.subtract(orderProduct.getPrice());
+
+        orderProduct.setOrder(null);
+    }
 }
