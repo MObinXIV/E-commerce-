da088039fbdb06b454a5d66f36337d09ed65fcf1
MObinXIV <mobinxiv@gmail.com>
2025-03-21 09:38:12 +0200

    add orderRepo

--- src/main/java/com/mobi/ecommerce/order/OrderRepository.java
+++ src/main/java/com/mobi/ecommerce/order/OrderRepository.java
@@ -1,2 +1,12 @@
-package com.mobi.ecommerce.order;public interface OrderRepository {
+package com.mobi.ecommerce.order;
+
+import com.mobi.ecommerce.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,UUID> {
 }
