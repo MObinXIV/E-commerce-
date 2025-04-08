c121e45b7b11377a8bfc393212a03491dedc9c86
MObinXIV <mobinxiv@gmail.com>
2025-04-08 16:22:54 +0200

    remove unnecessary function

--- src/main/java/com/mobi/ecommerce/product/ProductRepository.java
+++ src/main/java/com/mobi/ecommerce/product/ProductRepository.java
@@ -3,12 +3,10 @@ package com.mobi.ecommerce.product;
 import com.mobi.ecommerce.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-    List <Product> findByUserId(UUID userId);
     Optional <Product> findByIdAndUserId(UUID productId, UUID userId);
     Optional<Product> findByUserAndProductName(User user, String productName);
 }
