7395a97d9fafba6c2827f609f1613a8aed211f83
MObinXIV <mobinxiv@gmail.com>
2025-04-14 02:30:48 +0200

    handle no token authorization exception

--- src/main/java/com/mobi/ecommerce/exception/DefaultExceptionHandler.java
+++ src/main/java/com/mobi/ecommerce/exception/DefaultExceptionHandler.java
@@ -4,12 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 // it catches any exception & translate it into a payload suitable to a  client
 @ControllerAdvice
@@ -27,5 +28,34 @@ public class DefaultExceptionHandler {
                 LocalDateTime.now()
         );
            return new ResponseEntity<>(apiError, NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    public ResponseEntity<ApiError> handleException(
+            InsufficientAuthenticationException e,
+            HttpServletRequest request
+    ){
+        ApiError apiError= new ApiError(
+                request.getServletPath(),
+                e.getMessage(),
+                FORBIDDEN.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(
+            Exception e,
+            HttpServletRequest request
+    ){
+        ApiError apiError= new ApiError(
+                request.getServletPath(),
+                e.getMessage(),
+                INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, INTERNAL_SERVER_ERROR);
     }
 }
